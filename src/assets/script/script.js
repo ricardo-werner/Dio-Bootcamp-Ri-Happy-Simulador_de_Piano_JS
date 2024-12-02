e66d26f07452719e752bc541ef4cdfe21edf930e
Ricardo Werner <wernerdive@gmail.com>
2024-12-02 10:10:05 -0300

    feat: add selecao das teclas do piano e tocar os sons referentes as teclas capturadas

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,1 +1,15 @@
-console.log('Hello from script.js');
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+
+let audio = new Audio('src/assets/audios/a.wav')
+
+const playTune = (key) => {
+  audio.src = `src/assets/audios/${key}.wav`
+  audio.play();
+}
+
+pianoKeys.forEach((key) => {
+  console.log(key.dataset.key)
+
+  key.addEventListener('click', () => playTune(key.dataset.key));
+
+});
